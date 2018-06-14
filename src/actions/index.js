970a3e35db8ad2b56caad97dc4c492703c8edf57
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-14 19:15:55 -0400

    make an ajax call with axios for weather data

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'a0646c4d349dcf2b157058e98de63594';
+const root_url = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// Make it a constant to keep all types consistent (DRY code)
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${root_url}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
   constructor(props){
     super(props);
 
     this.state = {term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   // All dom event handles come with an event object
-  // May need to bind the context too
+  // Will NEED to bind the context too
   onInputChange(event){
     this.setState({ term: event.target.value });
   }
   onFormSubmit(event){
     event.preventDefault();
+
+    // Go fetch the weather data and clear
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render () {
@@ -34,3 +42,9 @@ export default class SearchBar extends Component{
     );
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
