b7fc34c5f1266fddbc97e4c1ab95714789623171
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-17 15:49:15 -0400

    got the weather reducer to return state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,13 @@
-export default function(state = null, action){
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+  // Switch stateemnt for handle weather action type
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // concat makes a new instance like setState
+      return state.concat([ action.payload.data ]);
+    default:
+      return state;
+  }
   return state;
 }
