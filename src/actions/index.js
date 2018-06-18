1509f7a66eb7a8345e4e56f3449dd5ff4f6590a3
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-18 07:30:01 -0400

    mapping city data list working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 const API_KEY = 'a0646c4d349dcf2b157058e98de63594';
-const root_url = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const root_url = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
 
 // Make it a constant to keep all types consistent (DRY code)
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${root_url}&q=${city},us`;
-  const request = axios.get(url);
+  const request = axios.get(
+    url
+  );
 
   console.log('Request: ', request);
 
